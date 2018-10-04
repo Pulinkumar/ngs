b71a0cc75a29915f2ff6369b3577a25941c2ea56
Pulinkumar <pulin.patel.it@gmail.com>
2018-10-04 14:10:21 -0400

    k

---
+++ 4-modules/1-define-use/3-wrapper.js
@@ -0,0 +1,9 @@
+//function(exports, module, require, __filename, __dirname){
+
+exports.p = 55;
+module.exports.r = 66;
+console.log(arguments);
+//console.log(exports, __filename, __dirname);
+
+//return module.exports;
+//}
---
+++ Note.txt
@@ -0,0 +1,1 @@
+https://edgecoders.com/how-well-do-you-know-node-js-36b1473c01c8
